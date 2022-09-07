22709edb2b1fe99b0ace8436881b042a85f77f66
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2022-09-07 23:33:30 +0300

    add camera follow player

---
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    [SerializeField] private Transform playerContainer;
+    [SerializeField] private float lerpMultiplier;
+    private Vector3 _offset;
+    
+    void Start()
+    {
+        _offset = transform.position - playerContainer.transform.position;
+    }
+    
+    void LateUpdate()
+    {
+        transform.position =
+            Vector3.Lerp(transform.position, playerContainer.position + _offset, lerpMultiplier * Time.deltaTime);
+    }
+}
