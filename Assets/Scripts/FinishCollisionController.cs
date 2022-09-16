8414d9944a945a780348d8f178563446f36a8dcc
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2022-09-16 18:21:55 +0300

    update finish collision controller

--- Assets/Scripts/FinishCollisionController.cs
+++ Assets/Scripts/FinishCollisionController.cs
@@ -2,7 +2,7 @@
 
 public class FinishCollisionController : MonoBehaviour
 {
-    /*[SerializeField] private PlayerMovement playerMovement;
+    [SerializeField] private PlayerMovement playerMovement;
     [SerializeField] private CharacterAnimationController characterAnimCont;
     
     private void Start()
@@ -23,5 +23,5 @@ public class FinishCollisionController : MonoBehaviour
             playerMovement.speedY = 0;
             playerMovement.speedZ = 0;
         }
-    }*/
+    }
 }
